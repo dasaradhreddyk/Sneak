e8d0f2af048299f4a21775fb978de6af95c829c0
dasaradhreddyk <58421595+dasaradhreddyk@users.noreply.github.com>
2022-07-28 21:47:58 +1000

    Add files via upload

---
+++ src/app/mockdata.ts
@@ -0,0 +1,13 @@
+export const CHARACTERS: any[] =
+[
+  {
+    name:  'http://facebook.com/e.melisma',
+    age: 'http://graph.facebook.com/100001447162898/picture?type=large#/100001447162898',
+    species: 'http://graph.facebook.com/100001447162898/picture?type=large#/100001447162898',
+            occupation: 'http://graph.facebook.com/100001447162898/picture?type=large#/100001447162898',
+            info1: '',
+            info2:''
+
+  },
+  
+]
